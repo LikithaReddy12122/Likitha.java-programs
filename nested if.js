3cbf9f7e5500e6bc746eeaea183756b9b9ed9015
Rajeswari <likithareddy875@gmail.com>
2026-03-11 16:10:12 +0530

    Add files via upload

---
+++ nested if.js
@@ -0,0 +1,15 @@
+let num = 5;
+if (num >= 0) 
+    {          
+    if (num % 2 === 0)
+         {
+        console.log(" Even");
+    }
+     else
+         {
+        console.log("Odd");
+    }
+} else 
+    {
+    console.log("Please enter a number");
+}
